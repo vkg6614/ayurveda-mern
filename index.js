126dd87825538e3cc66442be59ba9f7cc63e4459
vkg6614 <vkg6614@gmail.com>
2022-06-12 20:26:30 +0530

    eror

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("working");
+});
+
 app.use("/products", productRoute);
 
 mongoose
