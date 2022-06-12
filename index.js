1f8aa9b30819b6a93da1dbf9d95b56ab0c2facce
vkg6614 <vkg6614@gmail.com>
2022-06-12 21:24:19 +0530

    error solving

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const path = require("path");
 const productRoute = require("./routes/productRoutes.js");
 
 const app = express();
-dotenv.config("/");
+dotenv.config("/hgfh");
 
 const port = process.env.PORT || 5000;
 
